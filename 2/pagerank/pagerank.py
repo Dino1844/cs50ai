980c42feb8f34d4c087604b527acb44013062d4d
Alfred <fangjch2023@lzu.edu.cn>
2025-01-11 10:52:05 +0800

    add pagerank but iteraction has a bug

--- 2/pagerank/pagerank.py
+++ 2/pagerank/pagerank.py
@@ -57,7 +57,16 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    raise NotImplementedError
+    nexts = corpus[page]
+    new_dict = {key:0 for key in corpus.keys()}
+    h = len(nexts)
+    w = len(new_dict)
+    for next in nexts:
+        new_dict[next] += (damping_factor / h)
+    for key in corpus.keys():
+        new_dict[key] += ((1-damping_factor) / w)
+    
+    return new_dict
 
 
 def sample_pagerank(corpus, damping_factor, n):
@@ -69,9 +78,25 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
-
-
+    freq = {page:0 for page in corpus.keys()}
+    
+    # begin page
+    begin_page = random.sample(list(corpus.keys()),1)[0]
+    freq[begin_page] += 1
+    
+    # calculate the trans dict
+    trans = {page:transition_model(corpus,page,damping_factor) for page in corpus}
+    now_trans = trans[begin_page]
+    # pagerank pass
+    for i in range(1,n):
+        #important function
+        result = random.choices(population=list(now_trans.keys()), weights=list(now_trans.values()))[0]
+        freq[result] += 1
+        now_trans = trans[result]
+    s = sum(freq.values())
+    for key in freq.keys():
+        freq[key] /= s
+    return freq
 def iterate_pagerank(corpus, damping_factor):
     """
     Return PageRank values for each page by iteratively updating
@@ -81,8 +106,37 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    # freq init
+    l = len(corpus)
+    freq = {page:1/l for page in corpus.keys()}
+    
+    # get the father page
+    def scan_father(page,corpus):
+        father = []
+        for key in corpus.keys():
+            if page in corpus[key]:
+                father.append(key)
+        return father
+    father_page = {page:scan_father(page,corpus) for page in corpus.keys()}
 
+    def kill(last_freq,now_freq):
+        for page in last_freq.keys():
+            if(abs(last_freq[page] - now_freq[page]) < 0.001):
+                continue
+            else:
+                return False
+        return True
+    # 首先迭代100次, 然后再判断
+    num = 0
+    while True:
+        num += 1
+        freqcopy = freq.copy()
+        for page in corpus.keys():
+            freq[page] = (1-damping_factor) / l + \
+                        damping_factor * sum([freqcopy[father] for father in father_page[page]]) / len(father_page[page])
+        if num > 100 and kill(freqcopy, freq):
+            break
+    return freq
 
 if __name__ == "__main__":
     main()
